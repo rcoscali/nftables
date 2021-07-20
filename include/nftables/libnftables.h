9edaa6a51eab49a378dd358e0b4254d0398c629f
Pablo Neira Ayuso <pablo@netfilter.org>
2021-07-20 12:17:33 +0200

    src: add --define key=value
    This patch adds a new option to define variables from the command line.
    
     # cat test.nft
     table netdev x {
            chain y {
                    type filter hook ingress devices = $dev priority 0;
                    counter accept
            }
     }
     # nft --define dev="{ eth0, eth1 }" -f test.nft
    
    You can only combine it with -f/--filename.
    
    Signed-off-by: Pablo Neira Ayuso <pablo@netfilter.org>

--- include/nftables/libnftables.h
+++ include/nftables/libnftables.h
@@ -78,6 +78,8 @@ const char *nft_ctx_get_error_buffer(struct nft_ctx *ctx);
 int nft_ctx_add_include_path(struct nft_ctx *ctx, const char *path);
 void nft_ctx_clear_include_paths(struct nft_ctx *ctx);
 
+int nft_ctx_add_var(struct nft_ctx *ctx, const char *var);
+
 int nft_run_cmd_from_buffer(struct nft_ctx *nft, const char *buf);
 int nft_run_cmd_from_filename(struct nft_ctx *nft, const char *filename);
 
