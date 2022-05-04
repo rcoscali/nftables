rcoscali/nftables

commit 743b0e81371f0e5a6d70375e8efd7fbe9b00cae7

optimize: do not clone unsupported statement
Skip unsupported statements when building the statement matrix,
otherwise clone remains uninitialized.

Fixes: fb298877ece2 ("src: add ruleset optimization infrastructure")
Signed-off-by: Pablo Neira Ayuso <pablo@netfilter.org>

src/optimize.c
@@ -274,7 +274,7 @@ static int rule_collect_stmts(struct optimize_ctx *ctx, struct rule *rule)
 				clone->log.prefix = expr_get(stmt->log.prefix);
 			break;
 		default:
-			break;
+			continue;
 		}
 
 		ctx->stmt[ctx->num_stmts++] = clone;

commit ea1f1c9ff608fbca858e13b7c3242f20cdb9854d

optimize: memleak in statement matrix
Release clone object in case this statement is not supported.

Fixes: 743b0e81371f ("optimize: do not clone unsupported statement")
Signed-off-by: Pablo Neira Ayuso <pablo@netfilter.org>

src/optimize.c
@@ -304,6 +304,7 @@ static int rule_collect_stmts(struct optimize_ctx *ctx, struct rule *rule)
 			clone->nat.type_flags = stmt->nat.type_flags;
 			break;
 		default:
+			stmt_free(clone);
 			continue;
 		}
 