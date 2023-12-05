f892224083794bc39b289046c69bc91973636afe
Florian Westphal <fw@strlen.de>
2023-12-05 12:56:08 +0100

    parser: tcpopt: fix tcp option parsing with NUM + length field
    commit 59a33d08ab3a75b2ae370b6816942793f49fa8db upstream.
    
    tcp option 254 length ge 4
    
    ... will segfault.
    The crash bug is that tcpopt_expr_alloc() can return NULL if we cannot
    find a suitable template for the requested kind + field combination,
    so add the needed error handling in the bison parser.
    
    However, we can handle this.  NOP and EOL have templates, all other
    options (known or unknown) must also have a length field.
    
    So also add a fallback template to handle both kind and length, even
    if only a numeric option is given that nft doesn't recognize.
    
    Don't bother with output, above will be printed via raw syntax, i.e.
    tcp option @254,8,8 >= 4.
    
    Fixes: 24d8da308342 ("tcpopt: allow to check for presence of any tcp option")
    Reported-by: Maciej Żenczykowski <zenczykowski@gmail.com>
    Signed-off-by: Florian Westphal <fw@strlen.de>

--- src/tcpopt.c
+++ src/tcpopt.c
@@ -119,6 +119,13 @@ static const struct exthdr_desc tcpopt_mptcp = {
 		[TCPOPT_MPTCP_SUBTYPE]  = PHT("subtype", 16, 4),
 	},
 };
+
+static const struct exthdr_desc tcpopt_fallback = {
+	.templates	= {
+		[TCPOPT_COMMON_KIND]	= PHT("kind",   0, 8),
+		[TCPOPT_COMMON_LENGTH]	= PHT("length", 8, 8),
+	},
+};
 #undef PHT
 
 const struct exthdr_desc *tcpopt_protocols[] = {
@@ -133,6 +140,17 @@ const struct exthdr_desc *tcpopt_protocols[] = {
 	[TCPOPT_KIND_MPTCP]		= &tcpopt_mptcp,
 	[TCPOPT_KIND_FASTOPEN]		= &tcpopt_fastopen,
 };
+
+static void tcpopt_assign_tmpl(struct expr *expr,
+			       const struct proto_hdr_template *tmpl,
+			       const struct exthdr_desc *desc)
+{
+	expr->exthdr.op     = NFT_EXTHDR_OP_TCPOPT;
+
+	expr->exthdr.desc   = desc;
+	expr->exthdr.tmpl   = tmpl;
+	expr->exthdr.offset = tmpl->offset;
+}
 
 /**
  * tcpopt_expr_alloc - allocate tcp option extension expression
@@ -183,18 +201,26 @@ struct expr *tcpopt_expr_alloc(const struct location *loc,
 		desc = tcpopt_protocols[kind];
 
 	if (!desc) {
-		if (field != TCPOPT_COMMON_KIND || kind > 255)
+		if (kind > 255)
 			return NULL;
+
+		desc = &tcpopt_fallback;
+
+		switch (field) {
+		case TCPOPT_COMMON_KIND:
+		case TCPOPT_COMMON_LENGTH:
+			tmpl = &desc->templates[field];
+			break;
+		default:
+			tmpl = &tcpopt_unknown_template;
+			break;
+		}
 
 		expr = expr_alloc(loc, EXPR_EXTHDR, &integer_type,
 				  BYTEORDER_BIG_ENDIAN, 8);
 
-		desc = tcpopt_protocols[TCPOPT_NOP];
-		tmpl = &desc->templates[field];
-		expr->exthdr.desc   = desc;
-		expr->exthdr.tmpl   = tmpl;
-		expr->exthdr.op = NFT_EXTHDR_OP_TCPOPT;
 		expr->exthdr.raw_type = kind;
+		tcpopt_assign_tmpl(expr, tmpl, desc);
 		return expr;
 	}
 
@@ -204,11 +230,9 @@ struct expr *tcpopt_expr_alloc(const struct location *loc,
 
 	expr = expr_alloc(loc, EXPR_EXTHDR, tmpl->dtype,
 			  BYTEORDER_BIG_ENDIAN, tmpl->len);
-	expr->exthdr.desc   = desc;
-	expr->exthdr.tmpl   = tmpl;
-	expr->exthdr.op     = NFT_EXTHDR_OP_TCPOPT;
+
 	expr->exthdr.raw_type = desc->type;
-	expr->exthdr.offset = tmpl->offset;
+	tcpopt_assign_tmpl(expr, tmpl, desc);
 
 	return expr;
 }
